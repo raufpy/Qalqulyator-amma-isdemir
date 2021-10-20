23a8e96c0ebb4e4c0195e008dbd7f6705ff41281
raufpy <89349355+raufpy@users.noreply.github.com>
2021-10-21 00:40:28 +0400

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,82 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <iostream>
+#include<math.h>
+#include<conio.h>
+
+
+/*int main()
+{
+	float flo1{}, flo2{}, flocal{};
+	char op;
+	char symbol=251;
+		printf("Choose an operator: [+,-,/,*,%%,√]\n", symbol);
+		scanf_s(" %c",&op);
+		if (op == '+')
+		{
+			printf("Enter The First Number: ");
+			scanf_s("%f", &flo1);
+			printf("Enter The Second Number: ");
+			scanf_s("%f", &flo2);
+			flocal = flo1 + flo2;
+			printf("The Result Is: %f", flocal);
+		}
+		else if (op == '-')
+		{
+			printf("Enter The First Number: ");
+			scanf_s("%f", &flo1);
+			printf("Enter The Second Number: ");
+			scanf_s("%f", &flo2);
+			flocal = flo1 - flo2;
+			printf("The Result Is: %f", flocal);
+		}
+		else if (op == '*')
+		{
+			printf("Enter The First Number: ");
+			scanf_s("%f", &flo1);
+			printf("Enter The Second Number: ");
+			scanf_s("%f", &flo2);
+			flocal = flo1 * flo2;
+			printf("The Result Is: %i", flocal);
+		}
+		else if (op == '/')
+		{
+			printf("Enter The First Number: ");
+			scanf_s("%f", &flo1);
+			printf("Enter The Second Number: ");
+			scanf_s("%f", &flo2);
+			flocal = flo1 / flo2;
+
+			printf("The Result Is: %f", flocal);
+		}
+		else if (op == '%')
+		{
+			float percentage{}, value{}, total{};
+			printf("Choose the total value: ");
+				scanf_s("%f",&total);
+				printf("Choose the percentage value: ");
+				scanf_s("%f", &value);
+				percentage = value / total * 100;
+				printf("The Result Is: %f", percentage);
+		}
+		else if (op == symbol)
+		{
+			
+			double srt1, num1;
+			printf("Enter The Number To Find Square Root:\n");
+			scanf_s("%f", &num1);
+			
+			srt1 = sqrt(num1);
+			printf("The Square Root of %f Is %f", num1, srt1);
+
+		}	
+		else
+		{
+			printf("Invalid Input Detected");
+		}
+		return 0;
+}*/
+
 
 int main()
 {
@@ -73,6 +149,7 @@ int main()
 		}
 		return 0;
 }
+
 
 
 /*void removeSpaces(char* Buf1)
